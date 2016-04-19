4908a7328b8fce5dea5bf65e2b458c56e4f1c8c6
Mark Gentzel <markgentzel@gmail.com>
2016-04-19 12:11:04 -0400

    Initial Commit

---
+++ MimicLibIOSExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MimicLibIOSExample
+//
+//  Created by Mark Gentzel on 4/19/16.
+//  Copyright © 2016 vocalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MimicLibIOSExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MimicLibIOSExample
+//
+//  Created by Mark Gentzel on 4/19/16.
+//  Copyright © 2016 vocalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
